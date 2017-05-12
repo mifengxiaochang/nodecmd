37507b95e704905e7f80094d9f0d5b1d86dd9248
advence-liz <1012515183@qq.com>
2017-05-12 16:10:54 +0800

    提交课程副本

---
+++ ESlint-demo/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var CLIEngine = require("eslint").CLIEngine;
+
+var cli = new CLIEngine({
+    envs: ["browser", "mocha"],
+    useEslintrc: false,
+    rules: {
+        semi: 2
+    }
+});
+
+// lint the supplied text and optionally set
+// a filename that is displayed in the report
+var report = cli.executeOnText("test.js");
+console.dir(report);
